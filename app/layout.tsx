4b30d2b253153bf45451bc7ede9ec7129a3c8660
guiontrauma-maker <guiontrauma@gmail.com>
2026-08-18 16:28:49 -0600

    Integrate Google Ads tracking scripts
    Added Google Ads tracking scripts to the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 import FloatingWhatsApp from "@/components/contact/FloatingWhatsApp";
@@ -32,6 +33,20 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18397632611"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-ads-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18397632611');
+          `}
+        </Script>
+
         {children}
 
         <FloatingWhatsApp />
